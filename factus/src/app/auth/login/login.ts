e8be154e446cea4e1b384fc248b243fe383896c0
Carlos Javier Rodriguez Manchola <carlosjavi1887@gmail.com>
2026-08-06 16:16:52 -0500

    feat(factus): agregar proyecto Angular con pantalla de login inicial (#1)

---
+++ factus/src/app/auth/login/login.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+  }
+}
